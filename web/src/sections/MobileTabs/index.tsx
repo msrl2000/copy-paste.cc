2c25067e24c496876e1702177167d60d1b0f6339
msrl2000 <86884058+msrl2000@users.noreply.github.com>
2025-05-18 02:18:50 +0300

    Add files via upload

--- web/src/sections/MobileTabs/index.tsx
+++ web/src/sections/MobileTabs/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { IoQrCode, IoChatbubbles, IoHome, IoSettings } from 'react-icons/io5';
+import { IoQrCode, IoChatbubbles, IoHome, IoSettings, IoScanSharp } from 'react-icons/io5';
 import { observer } from 'mobx-react-lite';
 
 import styles from './index.module.scss';
 import { Tab } from './Tab.js';
 import { chatStore, networkStore } from '../../stores/index.js';
 import { NotificationCount } from '../../components/NotificationCount.js';
+import { applicationStore } from '../../stores/index.js';
 
 export const MobileTabs: React.FC = observer(() => {
   return (
