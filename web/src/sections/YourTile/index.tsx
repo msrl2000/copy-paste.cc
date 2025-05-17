53bab90cfd5e8a1062f4801859cb690b8f2f7699
msrl2000 <86884058+msrl2000@users.noreply.github.com>
2025-05-18 02:19:34 +0300

    Add files via upload

--- web/src/sections/YourTile/index.tsx
+++ web/src/sections/YourTile/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useTranslation } from 'react-i18not';
-import { IoQrCode, IoSettings } from 'react-icons/io5';
+import { IoQrCode, IoSettings, IoScanSharp } from 'react-icons/io5';
 
 import styles from './index.module.scss';
 import { TargetTile } from '../../components/TargetTile.js';
@@ -34,10 +34,26 @@ export const YourTileSection: React.FC = observer(() => {
               <IoQrCode />
             </IconButton>
             <IconButton
+              onClick={() => applicationStore.openModal('scan')}
+              title={t('qrScanner.title')}
+            >
+              <IoScanSharp />
+            </IconButton>
+            <IconButton
               onClick={() => applicationStore.openModal('settings')}
               title={t('tabs.settings')}
             >
               <IoSettings />
+            </IconButton>
+          </div>
+
+          {/* Mobile actions */}
+          <div className={clsx(styles.actions, 'desktopHidden')}>
+            <IconButton
+              onClick={() => applicationStore.openModal('scan')}
+              title="קרא QR"
+            >
+              <IoScanSharp />
             </IconButton>
           </div>
         </div>
